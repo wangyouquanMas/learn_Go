85b1ef1e74e50efa8330c0b9415fcefcf045575a
wang <wang@192.168.0.102>
2021-07-19 22:13:05 +0800

    update

--- http/Handle/main.go
+++ http/Handle/main.go
@@ -21,7 +21,6 @@ func (h *countHandler) ServeHTTP(w http.ResponseWriter,r * http.Request){
 
 
 func main(){
-	// new 会带动结构体内部元素的初始化？ 被结构体拥有的方法也会被初始化？
 	http.Handle("/count",new(countHandler))
 	log.Fatal(http.ListenAndServe(":8080",nil))
 }
