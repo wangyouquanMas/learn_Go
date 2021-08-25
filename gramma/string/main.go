8f331b268dc763834f223bedfae70e83ce4db630
wang <wang@wangdeMacBook-Pro.local>
2021-08-25 22:12:28 +0800

    1

--- gramma/string/main.go
+++ gramma/string/main.go
@@ -2,20 +2,24 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func main() {
 
-	var styleList []string
-	styleList = append(styleList, "a", "b", "c")
-	//根据sep将数组元素拼接成string，
-	fmt.Println(strings.Join(styleList, "*"))
-
-	// len
-
-	word := "/lallala/b/c"
-
-	fmt.Printf("%c", word[len(word)-1])
+	//var styleList []string
+	//styleList = append(styleList, "a", "b", "c")
+	////根据sep将数组元素拼接成string，
+	//fmt.Println(strings.Join(styleList, "*"))
+	//
+	//// len
+	//
+	//word := "/lallala/b/c"
+	//
+	//fmt.Printf("%c", word[len(word)-1])
 	//fmt.Print(len(word))
+
+	//string -> []byte
+	a := "{2021082416777216}-2-99"
+	fmt.Println([]byte(a))
+
 }
