390b2c7c4c33afc10a306b053cd1fe5f71dc0fda
wang <wang@wangdeMacBook-Pro.local>
2021-11-08 12:23:36 +0800

    feature(unmarshal) 结构体字段映射

--- gramma/unmarshal/main.go
+++ gramma/unmarshal/main.go
@@ -7,41 +7,37 @@ import (
 
 /*
 1 :Json Marshal：将数据编码成json字符串
+2 :  marshol的结构体 映射是根据字段名称来进行匹配的
+
+   比如在stu1中 对 Name, Age进行marshal, 然后unmarshal stu2时， Name,Age字段会被映射，而Test没有，所以为默认值。
+   也即是说， 支持字段个数不同的结构体的unmarshal， 只要有同名字段[区分大小写]，就会被映射
 */
-type Stu struct {
-	Name  string `json:"name"`
-	Age   int
-	style int
-}
-
-type Stu1 struct {
-	Name  string `json:"name"`
-	Age   int
-	style int
-}
-
-type Stu2 struct {
-	Name string `json:"name"`
-	Age  int
-}
 
 func main() {
 
-	stu := &Stu1{
+	type Stu1 struct {
+		Name string `json:"name"`
+		Age  int
+	}
+
+	type Stu2 struct {
+		Name string `json:"name"`
+		Age  int
+		Test int
+	}
+
+	stu3 := &Stu1{
 		Name: "Apple",
 		Age:  20,
 	}
 
-	//stu2 := &Stu{
-	//	Name:  "Apple",
-	//	Age:   20,
-	//	style: 1,
-	//}
+	var stu41 Stu2
 
-	stuByte, _ := json.Marshal(stu)
+	stuByte, _ := json.Marshal(stu3)
 	fmt.Println(string(stuByte))
 
-	json.Unmarshal(stuByte, *stu)
+	json.Unmarshal(stuByte, &stu41)
+	//json.Unmarshal(stuByte, *stu41)
+	fmt.Println((stu41))
 
-	fmt.Println((stu))
 }
